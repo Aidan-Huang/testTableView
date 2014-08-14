60623594666403f05018f6f3e63e0df96e2fd5e2
Aidan <admin@MBP.local>
2014-08-14 22:28:23 +0800

    Initial Commit

---
+++ testTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testTableView
+//
+//  Created by Aidan on 14-8-14.
+//  Copyright (c) 2014年 ideal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
